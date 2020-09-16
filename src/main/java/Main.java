5b4205954f41a087d7ac7bb63b7b3f9620e86aab
Buddhi <b.wathsala.bw@gmail.com>
2020-09-16 14:09:48 +0530

    Add run jar configs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,5 @@ public class Main {
         System.out.println("This is a sample project to test GitHub packages");
         System.out.println("Change 1");
         System.out.println("Remove snapshot");
-        System.out.println("Change 2");
     }
 }
